2d8b67a8968e7447586b0c462e63cd7b4cd0ffe4
Maurice Willems <mauricewillems@gmail.com>
2022-09-07 21:25:40 +0200

    feat: install hotwire Turbo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,7 @@
 import './bootstrap';
+import './elements/turbo-echo-stream-tag';
+import './libs/turbo';
+import './libs/alpine';
 
 import {createApp} from 'vue/dist/vue.esm-bundler';
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>SRVN Dash</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>SRVN Dash </title>
 
     @vite(['resources/js/app.js'])
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
 
     <livewire:styles />
 </head>
@@ -26,6 +28,8 @@
     </div>
 
     <livewire:scripts />
+    <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js"
+        data-turbolinks-eval="false" data-turbo-eval="false"></script>
 </body>
 
 </html>
