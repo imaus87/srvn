2613b43e970391c9017e101f7199182ede101089
Maurice Willems <mauricewillems@gmail.com>
2022-09-08 00:43:09 +0200

    feat: links to auth routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Fortify::loginView(function() {
+            return view('auth.login');
+        });
+
+        Fortify::registerView(function() {
+            return view('auth.register');
+        });
+
+        Fortify::requestPasswordResetLinkView(function() {
+            return view('auth.forgot-password');
+        });
+
+        Fortify::confirmPasswordView (function() {
+            return view('auth.confirm-password');
+        });
+
+        Fortify::requestPasswordResetLinkView(function() {
+            return view('auth.forgot-password');
+        });
+
+        Fortify::verifyEmailView(function() {
+            return view('auth.verify-email');
+        });
+
     }
 }
