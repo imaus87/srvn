5f176963c22e82cce06842a2fa07f2d44ffc3647
Andre Kolmeijer <andrekolmeijer@gmail.com>
2022-09-09 19:05:43 +0200

    build: added tailwind forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,13 @@ const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-    ],
-    theme: {
-        extend: {
-            fontFamily:{
-                sans: ['Montserrat', ...defaultTheme.fontFamily.sans]
-            },
-        },
-    },
-    plugins: [],
-};
+  content: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: [require('@tailwindcss/forms')]
+}
