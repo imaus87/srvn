281c632ef478008f016e75e1618908b099a12b76
Maurice Willems <mauricewillems@gmail.com>
2022-09-17 18:58:21 +0200

    feat: logout action

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public function logout()
+    {
+        Auth::logout();
+    }
+
     public function render()
     {
         return view('dashboard');
