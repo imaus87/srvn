c50bd0b777a62747c824ed6875e9b1b751d389de
Maurice Willems <mauricewillems@gmail.com>
2022-09-20 01:25:54 +0200

    build: add darkMode to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
   theme: {
     extend: {
