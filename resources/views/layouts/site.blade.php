4de54bfdd5915bedd278f7e4230f5bc0c9532f10
Maurice Willems <mauricewillems@gmail.com>
2022-09-20 01:27:40 +0200

    ref: changed title

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -5,15 +5,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>SRVN Dash </title>
+    <title>SimRace Vereniging Nederland</title>
 
     @vite(['resources/js/app.js'])
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet">
 
     <livewire:styles />
 
@@ -22,12 +22,12 @@
     <script defer src="https://unpkg.com/alpinejs@3.10.3/dist/cdn.min.js"></script>
 </head>
 
-<body class="antialiased font-sans text-stone-800 bg-[#f3f3f3] h-screen">
+<body class="h-screen bg-[#f3f3f3] font-sans text-stone-800 antialiased">
     <div id="app">
         <x-navbar />
-        <main class="px-6 py-9 h-screen">
+        <main class="h-screen px-6 py-9">
             <div class="mx-auto max-w-7xl">
-              {{ $slot }}
+                {{ $slot }}
             </div>
         </main>
 
