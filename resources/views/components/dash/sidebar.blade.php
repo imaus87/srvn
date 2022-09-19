598337183297421cb5462827816e0d3dff00a720
Maurice Willems <mauricewillems@gmail.com>
2022-09-20 01:29:47 +0200

    feat: add dash sindebar

---
+++ resources/views/components/dash/sidebar.blade.php
@@ -0,0 +1,25 @@
+<div class="h-full w-80 px-6 py-6">
+    <nav>
+        <ul class="space-y-2">
+            <li class="">
+                <a href="#" class="text-stone-00 flex items-center space-x-2 py-1 font-medium">
+                    <x-heroicon-s-user-group class="h-6 text-stone-700" />
+                    <span>Gebruikers</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="text-stone-00 flex items-center space-x-2 py-1 font-medium">
+                    <x-heroicon-s-users class="h-6 text-stone-700" />
+                    <span>Teams</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="text-stone-00 flex items-center space-x-2 py-1 font-medium">
+                    <x-heroicon-s-trophy class="h-6 text-stone-700" />
+                    <span>Kampioenschappen</span>
+                </a>
+            </li>
+
+        </ul>
+    </nav>
+</div>
