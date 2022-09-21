46dbc2cc7417517fc7f0e8e1114621bf6a34bc39
Maurice Willems <mauricewillems@gmail.com>
2022-09-21 21:41:59 +0200

    feat: menu to x-popover

--- resources/views/components/dash/navbar/desktop.blade.php
+++ resources/views/components/dash/navbar/desktop.blade.php
@@ -19,34 +19,10 @@
 
         <div class="flex items-center justify-center divide-x bg-transparent">
             <div class="relative"
-                 x-data="{
-                     open: false,
-                     toggle() {
-                         if (this.open) {
-                             return this.close()
-                         }
-
-                         this.$refs.button.focus()
-
-                         this.open = true
-                     },
-                     close(focusAfter) {
-                         if (!this.open) return
-
-                         this.open = false
-
-                         focusAfter && focusAfter.focus()
-                     }
-                 }"
-                 x-on:keydown.escape.prevent.stop="close($refs.button)"
-                 x-on:focusin.window="! $refs.panel.contains($event.target) && close()"
-                 x-id="['dropdown-button']">
-
+                 x-data
+                 x-popover>
                 <button class="flex space-x-2 rounded-md px-3 py-2 hover:bg-neutral-700 focus:bg-neutral-700 focus:ring focus:ring-neutral-600"
-                        x-ref="button"
-                        x-on:click="toggle()"
-                        x-bind:aria-expanded="open"
-                        x-bind:aria-controls="$id('dropdown-button')"
+                        x-popover:button
                         type="button">
                     <span>
                         {{ str(auth()->user()->name) }}
@@ -55,12 +31,9 @@
                     <x-heroicon-s-chevron-down class="h-5 w-5 text-neutral-300" />
                 </button>
 
-                <div x-ref="panel"
-                     x-show="open"
-                     x-transition.origin.top.left
-                     x-on:click.outside="close($refs.button)"
-                     x-bind:id="$id('dropdown-button')"
-                     style="display: none;"
+                <div x-popover:panel
+                     x-cloak
+                     x-transition.origin.top.center
                      class="absolute left-0 right-0 mt-2 rounded-md bg-white shadow-md dark:bg-neutral-700">
                     <a href="#"
                        class="block w-full px-4 py-2.5 text-left text-sm first-of-type:rounded-t-md last-of-type:rounded-b-md hover:bg-neutral-600 disabled:text-gray-500">
