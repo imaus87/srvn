64a644d8eecae31d3cf9ff948ac71d9d5d264f11
Andre Kolmeijer <andrekolmeijer@gmail.com>
2022-09-22 18:38:29 +0200

    ref: sidebar desktop

--- resources/views/components/dash/sidebar.blade.php
+++ resources/views/components/dash/sidebar.blade.php
@@ -1,24 +1,24 @@
 <div class="h-full w-80 px-6">
     <nav>
-        <ul class="space-y-2">
+        <ul class="space-y-2 font-medium text-neutral-500 dark:text-neutral-500">
             <li class="">
                 <a href="#"
-                   class="flex items-center space-x-2 py-1 font-medium text-neutral-500 dark:text-neutral-500">
-                    <x-heroicon-s-user-group class="h-6 text-neutral-300 dark:text-neutral-700" />
+                   class="flex items-center space-x-4 py-1">
+                    <x-heroicon-s-user-group class="h-6 w-6 text-neutral-400 dark:text-neutral-700" />
                     <span>Gebruikers</span>
                 </a>
             </li>
             <li>
                 <a href="#"
-                   class="flex items-center space-x-2 py-1 font-medium text-neutral-500 dark:text-neutral-500">
-                    <x-heroicon-s-users class="h-6 text-neutral-300 dark:text-neutral-700" />
+                   class="flex items-center space-x-4 py-1">
+                    <x-heroicon-s-users class="h-6 w-6 text-neutral-400 dark:text-neutral-700" />
                     <span>Teams</span>
                 </a>
             </li>
             <li>
                 <a href="#"
-                   class="flex items-center space-x-2 py-1 font-medium text-neutral-500 dark:text-neutral-500">
-                    <x-heroicon-s-trophy class="h-6 text-neutral-300 dark:text-neutral-700" />
+                   class="flex items-center space-x-4 py-1">
+                    <x-heroicon-s-trophy class="h-6 w-6 text-neutral-400 dark:text-neutral-700" />
                     <span>Kampioenschappen</span>
                 </a>
             </li>
