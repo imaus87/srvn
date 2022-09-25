a540adf26b7e2009cc88c9aed74d6688e59c8a28
Maurice Willems <mauricewillems@gmail.com>
2022-09-25 11:21:26 +0200

    feat:add maus own password and added Rene Hoogterp

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,12 +17,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+
         DB::table('users')->insert([
             'first_name' => 'Maurice',
             'last_name' => 'Willems',
             'full_name' => 'Maurice Willems',
             'email' => 'mauricewillems@gmail.com',
-            'password' => '$2y$10$BZOQqA/tuKbkjQ0ehdU0/eO6.9GdaDS9Y0OzrrlNoDLlGPOMG1YVm', //password
+            'password' => '$2y$10$qB4T3r//NxeGKcUxfk1kcezCU/w2mmoOLxNzg.2CEEVTV7nNys20q',
             'created_at' => Carbon::now(),
         ]);
 
@@ -32,6 +33,15 @@ class UserSeeder extends Seeder
             'full_name' => 'André Kolmeijer',
             'email' => 'andrekolmeijer@gmail.com',
             'password' => '$2y$10$sDzAGePl6yphWQF9CrdDF.UQUjYpoIgBOgYsAxg4DkOa7FOW8APgu',
+            'created_at' => Carbon::now(),
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'René',
+            'last_name' => 'Hoogterp',
+            'full_name' => 'René Hoogterp',
+            'email' => 'renehoogterp@srvn.com',
+            'password' => '$2y$10$BZOQqA/tuKbkjQ0ehdU0/eO6.9GdaDS9Y0OzrrlNoDLlGPOMG1YVm', //password
             'created_at' => Carbon::now(),
         ]);
     }
