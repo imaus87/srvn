be524092d5649f20a854a8ec24fc5380e5d726d0
Andre Kolmeijer <andrekolmeijer@gmail.com>
2022-09-27 16:20:20 +0200

    ref: added locale to faker

--- app/Providers/FakerServiceProvider.php
+++ app/Providers/FakerServiceProvider.php
@@ -17,7 +17,7 @@ class FakerServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(Generator::class, function () {
-            $faker = Factory::create();
+            $faker = Factory::create(config('app.faker_locale'));
             $faker->addProvider(new FakerPicsumImagesProvider($faker));
             return $faker;
         });
