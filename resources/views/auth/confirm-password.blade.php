f28f1680da7c9e503fa5ae70d9dd850f641f3295
Andre Kolmeijer <andrekolmeijer@gmail.com>
2022-11-23 19:04:35 +0100

    ref: adjusted some of the styling

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,29 +1,37 @@
 <x-auth-layout>
     <x-auth.container>
         <x-slot name="logo">
-            <a href="{{ route('home') }}">
-                <x-logo.900 class="h-20" />
-            </a>
+            <x-auth.header />
         </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
+        <p class="mt-6 text-neutral-800">
+            {{-- TZT AANPASSEN --}}
             {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
-        </div>
+        </p>
 
         <!-- Validation Errors -->
         <x-auth.validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('password.confirm') }}">
+        <form method="POST"
+              action="{{ route('password.confirm') }}"
+              class="space-y-6">
             @csrf
 
             <!-- Password -->
-            <div>
-                <x-input.label for="password" :value="__('Password')" />
+            <x-input.label for="password"
+                           :value="__('Wachtwoord')"
+                           class="hidden" />
 
-                <x-input.text id="password" class="mt-1 block w-full" type="password" name="password" required
-                              autocomplete="current-password" />
-            </div>
+            <x-input.text id="password"
+                          class="block w-full"
+                          type="password"
+                          name="password"
+                          required
+                          autocomplete="current-password"
+                          placeholder="Wachtwoord"
+                          autofocus />
 
+            {{-- TZT AANPASSEN --}}
             <div class="mt-4 flex justify-end">
                 <x-button>
                     {{ __('Confirm') }}
