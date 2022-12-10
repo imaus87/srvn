757e9fbb8600550b2e4deeed4275a0bf81661aa2
Maurice Willems <mauricewillems@gmail.com>
2022-12-10 12:07:41 +0100

    test: configurations

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -76,5 +76,20 @@ class RoleAndPermissionSeeder extends Seeder
                 'role_id' => $superadmin,
                 'permission_id' => $editRoles,
             ]);
+
+        // Granting Roles
+        app('db')
+            ->table('model_has_roles')->insert([
+                'role_id' => 1,
+                'model_id' => 1,
+                'model_type' => 'App\Models\User'
+            ]);
+
+        app('db')
+            ->table('model_has_roles')->insert([
+                'role_id' => 1,
+                'model_id' => 2,
+                'model_type' => 'App\Models\User'
+            ]);
     }
 }
