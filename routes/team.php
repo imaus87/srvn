8525ff140c4531ac63e8405705cbe6b5d12cf179
Maurice Willems <mauricewillems@gmail.com>
2022-12-13 16:25:10 +0100

    build: updated routes

--- routes/team.php
+++ routes/team.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Livewire\Dash\Team\Dashboard;
+use App\Http\Livewire\Dash\Team\Information;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', Dashboard::class)->name('dashboard');
+
+Route::get('/info', Information::class)->name('info');
