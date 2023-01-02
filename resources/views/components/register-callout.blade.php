f900db843a34ffeaf43aa7c9f2f723e0a67e4dc5
Andre Kolmeijer <andrekolmeijer@gmail.com>
2023-01-02 11:24:58 +0100

    feat: register callout componnent

--- resources/views/components/register-callout.blade.php
+++ resources/views/components/register-callout.blade.php
@@ -1,5 +1,164 @@
-<div class="bg-neutral-900 text-neutral-300">
-    <div class="mx-auto max-w-7xl px-6 py-12 w-full h-60">
-        Klaar om in te stappen?
+<section class="bg-neutral-900 text-neutral-300 sm:rounded-lg">
+    <div class="mx-auto w-full max-w-7xl px-6 py-12">
+
+        <div class="pb-9 text-center">
+            <x-h3 class="text-2xl font-semibold lg:text-3xl">
+                Stap in bij dé <span class="font-extrabold">nederlandse</span> simrace vereniging!
+            </x-h3>
+        </div>
+
+        <ul class="grid grid-cols-1 gap-3 text-lg sm:grid-cols-2 sm:px-6">
+            <li class="space-y-3 rounded-md bg-neutral-500/5 p-6 shadow-md">
+                <div class="flex items-center space-x-3">
+                    <x-heroicon-s-trophy class="h-7 w-7" />
+                    <x-h4>
+                        Officiël kampioenschap
+                    </x-h4>
+                </div>
+                    <p class="text-neutral-500 ml-10">
+                        Rijd twee maal per jaar mee in het officiël nederlands kampioenschap simracen.
+                    </p>
+            </li>
+            <li class="space-y-3 rounded-md bg-neutral-500/5 p-6 shadow-md">
+                <div class="flex items-center space-x-3">
+                    <x-heroicon-s-user-group class="h-7 w-7" />
+                    <x-h4>
+                        Winter- en zomerstop
+                    </x-h4>
+                </div>
+                    <p class="text-neutral-500 ml-10">
+                        Met de winter en somer series tussen de seizoenen in verveel je je geen moment!
+                    </p>
+            </li>
+            <li class="space-y-3 rounded-md bg-neutral-500/5 p-6 shadow-md">
+                <div class="flex items-center space-x-3">
+                    <x-heroicon-s-paint-brush class="h-7 w-7" />
+                    <x-h4>
+                        Eigen liveries
+                    </x-h4>
+                </div>
+                    <p class="text-neutral-500 ml-10">
+                        Ontwerp en rijd rond met je eigen gemaakte livery of gebruik er een van ons.
+                    </p>
+            </li>
+            <li class="space-y-3 rounded-md bg-neutral-500/5 p-6 shadow-md">
+                <div class="flex items-center space-x-3">
+                    <x-heroicon-s-user-circle class="h-7 w-7" />
+                    <x-h4>
+                        Gratis account
+                    </x-h4>
+                </div>
+                    <p class="text-neutral-500 ml-10">
+                        Schrijf je nu in voor een gratis account en verken de wonderen wereld van het simracen!
+                    </p>
+            </li>
+            {{-- <li class="space-y-3 rounded-md border border-srvn-orange/90 p-6">
+                <x-h4>
+                    Overtuigd?
+                </x-h4>
+                <p class="text-neutral-500">
+                    Schrijf je nu in voor een gratis account.
+                </p>
+            </li> --}}
+            {{-- <li class="">
+                <x-h3 class="text-2xl font-semibold lg:text-3xl">
+                    Doe je gordel om en stap in bij dé<br class="hidden lg:block"> nederlandse simrace vereniging!
+                </x-h3>
+            </li> --}}
+        </ul>
+
+        <div class="flex items-center justify-center pt-6">
+            <x-button
+                      class="w-full max-w-lg border-srvn-orange/40 bg-srvn-orange/90 hover:border-srvn-orange/90 hover:text-neutral-300">
+                Klaar om in te stappen?
+                {{-- Lees meer over hoe SRVN werkt / wij werken --}}
+            </x-button>
+        </div>
+
     </div>
-</div>
+
+    {{-- <div class="mx-auto w-full max-w-7xl px-6 py-12">
+
+        <div class="pb-9 text-center">
+            <x-h3 class="text-2xl font-semibold lg:text-3xl">
+                Stap in bij dé <span class="font-extrabold">nederlandse</span> simrace vereniging!
+            </x-h3>
+        </div>
+
+        <ul class="grid grid-cols-1 gap-3 text-lg sm:grid-cols-2 sm:px-6">
+            <li class="space-y-6 rounded-md bg-neutral-500/5 p-6 shadow-md">
+                <div class="flex items-center justify-center">
+                    <x-heroicon-s-trophy class="h-24 w-24" />
+                </div>
+                <div class="space-y-3">
+                    <x-h4 class="text-center">
+                        Officiël kampioenschap
+                    </x-h4>
+                    <p class="text-neutral-500 px-6">
+                        Rijd twee maal per jaar mee in het officiël nederlands kampioenschap simracen.
+                    </p>
+                </div>
+            </li>
+            <li class="space-y-3 rounded-md bg-neutral-500/5 p-6 shadow-md">
+                <div class="flex items-center justify-center">
+                    <x-heroicon-s-user-group class="h-24 w-24" />
+                </div>
+                <div class="space-y-3">
+                    <x-h4 class="text-center">
+                        Winter- en zomerstop
+                    </x-h4>
+                    <p class="text-neutral-500 px-6">
+                        Met de winter en somer series tussen de seizoenen in verveel je je geen moment!
+                    </p>
+                </div>
+            </li>
+            <li class="space-y-3 rounded-md bg-neutral-500/5 p-6 shadow-md">
+                <div class="flex items-center justify-center">
+                    <x-heroicon-s-paint-brush class="h-24 w-24" />
+                </div>
+                <div class="space-y-3">
+                    <x-h4 class="text-center">
+                        Eigen liveries
+                    </x-h4>
+                    <p class="text-neutral-500 px-6">
+                        Ontwerp en rijd rond met je eigen gemaakte livery of gebruik er een van ons.
+                    </p>
+                </div>
+            </li>
+            <li class="space-y-3 rounded-md bg-neutral-500/5 p-6 shadow-md">
+                <div class="flex items-center justify-center">
+                    <x-heroicon-s-user-circle class="h-24 w-24" />
+                </div>
+                <div class="space-y-3">
+                    <x-h4 class="text-center">
+                        Gratis account
+                    </x-h4>
+                    <p class="text-neutral-500 px-6">
+                        Schrijf je nu in voor een gratis account en verken de wonderen wereld van het simracen!
+                    </p>
+                </div>
+            </li> --}}
+            {{-- <li class="space-y-3 rounded-md border border-srvn-orange/90 p-6">
+                <x-h4>
+                    Overtuigd?
+                </x-h4>
+                <p class="text-neutral-500">
+                    Schrijf je nu in voor een gratis account.
+                </p>
+            </li> --}}
+            {{-- <li class="">
+                <x-h3 class="text-2xl font-semibold lg:text-3xl">
+                    Doe je gordel om en stap in bij dé<br class="hidden lg:block"> nederlandse simrace vereniging!
+                </x-h3>
+            </li> --}}
+        {{-- </ul>
+
+        <div class="flex items-center justify-center pt-6">
+            <x-button
+                      class="w-full max-w-lg border-srvn-orange/40 bg-srvn-orange/90 hover:border-srvn-orange/90 hover:text-neutral-300">
+                Account aanmaken
+            </x-button>
+        </div>
+
+    </div> --}}
+</section>
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,4 +1,49 @@
 <div>
-    Home
+
+    <x-register-callout />
+
+    <section class="mt-9">
+        <x-h3 class="pl-6">Verder neuzen</x-h3>
+        <nav>
+            <ul class="flex snap-x snap-mandatory gap-3 overflow-x-auto py-3 scrollbar-hide">
+                <li class="flex snap-center snap-always first:snap-start first:pl-6 last:snap-end last:pr-6">
+                    <a href="#"
+                       class="flex w-72 grow flex-col items-center rounded-md bg-white p-6 shadow-md">
+                        <img src="https://www.srvn.nl/klassement/design/liveries_2019-1/dtcc/Rene-Hoogterp.gif"
+                             alt="GNCR Motorsports Logo"
+                             class="grow">
+                        <span class="inline-block w-full flex-none text-center">Kalender</span>
+                    </a>
+                </li>
+                <li class="flex snap-center snap-always first:snap-start first:pl-6 last:snap-end last:pr-6">
+                    <a href="#"
+                       class="flex w-72 grow flex-col items-center rounded-md bg-white p-6 shadow-md">
+                        <img src="../img/anillusion-racing.svg"
+                             alt="Anillusion Racing Logo"
+                             class="grow">
+                        <span class="inline-block w-full flex-none text-center">Klassement</span>
+                    </a>
+                </li>
+                <li class="flex snap-center snap-always first:snap-start first:pl-6 last:snap-end last:pr-6">
+                    <a href="#"
+                       class="flex w-72 grow flex-col items-center rounded-md bg-white p-6 shadow-md">
+                        <img src="https://www.srvn.nl/klassement/design/liveries_2019-1/dtcc/Rene-Hoogterp.gif"
+                             alt="GNCR Motorsports Logo"
+                             class="grow">
+                        <span class="inline-block w-full flex-none text-center">Rijders</span>
+                    </a>
+                </li>
+                <li class="flex snap-center snap-always first:snap-start first:pl-6 last:snap-end last:pr-6">
+                    <a href="#"
+                       class="flex w-72 grow flex-col items-center rounded-md bg-white p-6 shadow-md">
+                        <img src="../img/anillusion-racing.svg"
+                             alt="Anillusion Racing Logo"
+                             class="grow">
+                        <span class="inline-block w-full flex-none text-center">Teams</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </section>
 
 </div>
