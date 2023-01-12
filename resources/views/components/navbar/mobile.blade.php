0ee97acae5b45a738895ed38cb05e3e641276f62
Andre Kolmeijer <andrekolmeijer@gmail.com>
2023-01-12 13:29:40 +0100

    feat: added app download section, adjusted animation

--- resources/views/components/navbar/mobile.blade.php
+++ resources/views/components/navbar/mobile.blade.php
@@ -38,37 +38,37 @@
 
         {{-- Internal Links --}}
         <nav class="shrink grow-0 basis-auto bg-white px-3 pt-3 md:px-6">
-            <h2 x-bind:class="navOpen && '!opacity-100 !translate-x-0 !delay-[100ms]'"
+            <h2 x-bind:class="navOpen && '!opacity-100 !translate-x-0 !delay-[83ms]'"
                 class="translate-x-[-5%] pb-6 text-sm font-medium uppercase text-neutral-400 opacity-0 transition-all delay-[100ms] duration-500">
                 Intern
             </h2>
             <ul class="flex flex-col space-y-3">
-                <li x-bind:class="navOpen && '!opacity-100 !translate-x-0 !delay-[200ms]'"
-                    class="flex translate-x-[-5%] items-center opacity-0 transition-all delay-[200ms] duration-500">
+                <li x-bind:class="navOpen && '!opacity-100 !translate-x-0 !delay-[166ms]'"
+                    class="flex translate-x-[-5%] items-center opacity-0 transition-all delay-[166ms] duration-500">
                     <a class="{{ Route::is('news') ? 'text-srvn-orange font-medium' : '' }} flex w-full items-center space-x-2 hover:font-medium hover:text-srvn-orange"
                        href="{{ route('news') }}">
                         <x-heroicon-o-chevron-double-right class="-ml-[3px] h-5 text-srvn-orange" />
                         <span>Nieuws</span>
                     </a>
                 </li>
-                <li x-bind:class="navOpen && '!opacity-100 !translate-x-0 !delay-[200ms]'"
-                    class="flex translate-x-[-5%] items-center opacity-0 transition-all delay-[200ms] duration-500">
+                <li x-bind:class="navOpen && '!opacity-100 !translate-x-0 !delay-[249ms]'"
+                    class="flex translate-x-[-5%] items-center opacity-0 transition-all delay-[249ms] duration-500">
                     <a class="{{ Route::is('schedule') ? 'text-srvn-orange font-medium' : '' }} flex w-full items-center space-x-2 hover:font-medium hover:text-srvn-orange"
                        href="{{ route('schedule') }}">
                         <x-heroicon-o-chevron-double-right class="-ml-[3px] h-5 text-srvn-orange" />
                         <span>Kalender</span>
                     </a>
                 </li>
-                <li x-bind:class="navOpen && '!opacity-100 !translate-x-0 !delay-[300ms]'"
-                    class="flex translate-x-[-5%] items-center opacity-0 transition-all delay-[300ms] duration-500">
+                <li x-bind:class="navOpen && '!opacity-100 !translate-x-0 !delay-[333ms]'"
+                    class="flex translate-x-[-5%] items-center opacity-0 transition-all delay-[333ms] duration-500">
                     <a class="{{ Route::is('standings') ? 'text-srvn-orange font-medium' : '' }} flex w-full items-center space-x-2 hover:font-medium hover:text-srvn-orange"
                        href="{{ route('standings') }}">
                         <x-heroicon-o-chevron-double-right class="-ml-[3px] h-5 text-srvn-orange" />
                         <span>Klassement</span>
                     </a>
                 </li>
-                <li x-bind:class="navOpen && '!opacity-100 !translate-x-0 !delay-[400ms]'"
-                    class="flex translate-x-[-5%] items-center opacity-0 transition-all delay-[400ms] duration-500">
+                <li x-bind:class="navOpen && '!opacity-100 !translate-x-0 !delay-[416ms]'"
+                    class="flex translate-x-[-5%] items-center opacity-0 transition-all delay-[416ms] duration-500">
                     <a class="{{ Route::is('drivers') ? 'text-srvn-orange font-medium' : '' }} flex w-full items-center space-x-2 hover:font-medium hover:text-srvn-orange"
                        href="{{ route('drivers') }}">
                         <x-heroicon-o-chevron-double-right class="-ml-[3px] h-5 text-srvn-orange" />
@@ -94,16 +94,17 @@
                     <span>Mijn SRVN</span>
                 </a>
             </div> --}}
-            <div x-bind:class="navOpen && '!opacity-100 !delay-[0ms]'"
-                 class="w-full pt-6 pb-9 opacity-0 transition-opacity delay-[0ms] duration-500">
+            <div class="w-full pt-6 pb-9">
                 <ul class="flex flex-col items-center justify-center space-y-6">
-                    <li>
+                    <li x-bind:class="navOpen && '!opacity-100 !delay-[250ms]'"
+                        class="opacity-0 transition-opacity delay-[250ms] duration-250">
                         <a href="{{ route('register') }}"
                            class="font-semibold hover:text-srvn-orange">
                             Stap in
                         </a>
                     </li>
-                    <li class="w-full">
+                    <li x-bind:class="navOpen && '!opacity-100 !delay-[0ms]'"
+                        class="w-full opacity-0 transition-opacity delay-[0ms] duration-500">
                         <a href="{{ route('driver.dashboard') }}"
                            class="flex w-full items-center justify-center space-x-1 rounded border-2 border-srvn-orange bg-srvn-orange px-3 py-2 font-semibold text-white transition duration-150 ease-in-out hover:bg-transparent hover:text-neutral-800">
                             {{-- <x-heroicon-o-arrow-right-on-rectangle class="h-5" /> --}}
@@ -111,7 +112,8 @@
                             {{-- Schrijf in --}}
                         </a>
                     </li>
-                    <div class="flex items-center space-x-3">
+                    <div x-bind:class="navOpen && '!opacity-100 !delay-[250ms]'"
+                         class="flex items-center space-x-3 opacity-0 transition-opacity delay-[250ms] duration-250">
                         <li>
                             <a href="#"
                                class="hover:text-srvn-orange">
@@ -137,7 +139,7 @@
 
         {{-- External Links --}}
         <nav class="flex shrink grow basis-auto flex-col justify-between bg-srvn-gray">
-            <div class="shrink-0 grow basis-auto px-3 md:px-6">
+            <div class="shrink grow-0 basis-auto px-3 md:px-6">
                 <h2 x-bind:class="navOpen && '!opacity-100 !translate-x-0 !delay-[125ms]'"
                     class="translate-x-[-5%] pt-9 pb-6 text-sm font-medium uppercase text-neutral-400 opacity-0 transition-all delay-[125ms] duration-500">
                     Extern
@@ -171,6 +173,31 @@
                         </a>
                     </li>
                 </ul>
+            </div>
+
+            <div x-bind:class="navOpen && '!opacity-100 !delay-[250ms]'"
+                 class="mt-6 flex shrink-0 grow justify-center opacity-0 transition-opacity delay-[250ms] duration-250">
+                <div class="shrink grow-0">
+                    <div class="hidden items-center space-x-3 rounded-md bg-white p-3 shadow-md md:flex">
+                        <x-qr-code />
+                        <div class="pr-3">
+                            <p class="shrink-0 text-neutral-500">
+                                Scan om te installeren
+                                {{-- Scan om te Downloaden --}}
+                            </p>
+                            <p class="text-lg font-semibold">
+                                iOS & Android
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex items-center justify-center md:hidden">
+                    <a href="#">
+                        <img src="/img/app-store-badge.svg"
+                             alt="App Store Badge"
+                             class="h-12 shadow-md">
+                    </a>
+                </div>
             </div>
 
             {{-- Callout --}}
