fe4fbade7ed19cbb5e786014ea1bb1c1d8397da8
Andre Kolmeijer <andrekolmeijer@gmail.com>
2023-01-16 11:48:07 +0100

    feat: store badge component

---
+++ resources/views/components/store-badge.blade.php
@@ -0,0 +1,7 @@
+<div {{ $attributes->merge(['class' => 'flex items-center justify-center']) }}>
+    <a href="#">
+        <img src="/img/app-store-badge.svg"
+             alt="App Store Badge"
+             class="h-12 shadow-md">
+    </a>
+</div>
